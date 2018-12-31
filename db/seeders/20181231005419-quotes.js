baa78f85264c2735b555813fd8f92b572e4577c2
violet moon <violet.wmoon@gmail.com>
2018-12-30 20:17:44 -0600

    connected db to pgadmin using docker and seeded db

---
+++ db/seeders/20181231005419-quotes.js
@@ -0,0 +1,12 @@
+'use strict';
+const quotes = require('../../quotes.js')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Quotes', quotes, {})   
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('Quotes', null, {})
+  }
+}
---
+++ quotes.js
@@ -0,0 +1,26 @@
+module.exports = [
+	{
+		body: 'We cannot force the development of mindfulness.',
+		author: 'Allen Lokos',
+	},
+	{
+		body: 'Rejoicing in ordinary things is not sentimental or trite. It actually takes guts.',
+		author: 'Pema Chödrön',
+	},
+	{
+		body: 'Meditation practice isn’t about trying to throw ourselves away and become something better. It’s about befriending who we are already.',
+		author: 'Pema Chödrön',
+	},
+	{
+		body: 'Much of spiritual life is self-acceptance, maybe all of it.',
+		author: 'Jack Kornfield',
+	},
+	{
+		body: 'Many people are alive but don’t touch the miracle of being alive.',
+		author: 'Thích Nhất Hạnh',
+	},
+	{
+		body: 'To think in terms of either pessimism or optimism oversimplifies the truth. The problem is to see reality as it is',
+		author: 'Thích Nhất Hạnh',
+	},
+]
