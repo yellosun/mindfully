ae6c16f4d5c6a7593a6087cae01a96dad477b746
violet moon <violet.wmoon@gmail.com>
2018-12-30 18:48:09 -0600

    restructered framework

---
+++ db/models/quote.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Quote = sequelize.define('Quote', {
+    body: DataTypes.STRING,
+    author: DataTypes.STRING
+  }, {});
+  Quote.associate = function(models) {
+    // associations can be defined here
+  };
+  return Quote;
+};
